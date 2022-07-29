567732a0b620d6cf575a39ecd2b930dddad50ce0
NickSettler <moiseevnikita14@gmail.com>
2022-07-29 21:04:28 +0200

    (feat) add event system subscriber type and subscribers field

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,13 @@ module.exports = {
     node: true,
     jest: true,
   },
-  extends: ['airbnb-base', 'prettier'],
+  extends: [
+    'airbnb-base',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
@@ -12,6 +18,8 @@ module.exports = {
   },
   plugins: ['@typescript-eslint', 'prettier'],
   rules: {
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': ['error'],
     'prettier/prettier': 'error',
     'import/extensions': 0,
     'import/no-extraneous-dependencies': [
--- src/event-system.ts
+++ src/event-system.ts
@@ -1,5 +1,20 @@
+export enum EVENT_SYSTEM_EVENT_NAMES {
+  LOG_EVENT = 'log_event',
+}
+
+export interface EventSystemEvents {
+  [EVENT_SYSTEM_EVENT_NAMES.LOG_EVENT]: (message: string) => void;
+}
+
+export interface EventSystemSubscriber<K extends EVENT_SYSTEM_EVENT_NAMES> {
+  event: K;
+  handler: (...args: Parameters<EventSystemEvents[K]>) => void;
+}
+
 export default class EventSystem {
   private static instance: typeof this.prototype;
+
+  private subscribers: EventSystemSubscriber<EVENT_SYSTEM_EVENT_NAMES>[] = [];
 
   public static getInstance() {
     if (!EventSystem.instance) EventSystem.instance = new EventSystem();
