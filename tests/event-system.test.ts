f30cca80a9a130dc8ef0813d38646e36c59c039d
NickSettler <moiseevnikita14@gmail.com>
2022-07-29 22:50:40 +0200

    (refactor) updates tests

--- tests/event-system.test.ts
+++ tests/event-system.test.ts
@@ -1,9 +1,7 @@
 import EventSystem from '../src';
 import { EVENT_SYSTEM_EVENT_NAMES } from '../src/event-system';
 
-const emptyHandler = () => {
-  // ...
-};
+const echoHandler = (message: string) => message;
 
 describe('Event System initialization', () => {
   test('Event System getInstance() method works', () => {
@@ -15,22 +13,22 @@ describe('Event System subscription worker', () => {
   test("Event System's subscribe() method works", () => {
     const eventSystem = EventSystem.getInstance();
 
-    eventSystem.subscribe(EVENT_SYSTEM_EVENT_NAMES.LOG_EVENT, emptyHandler);
+    eventSystem.subscribe(EVENT_SYSTEM_EVENT_NAMES.LOG_EVENT, echoHandler);
 
     /* eslint-disable dot-notation */
     expect(eventSystem['subscribers'].length).toBe(1);
     expect(eventSystem['subscribers'][0].event).toBe(
       EVENT_SYSTEM_EVENT_NAMES.LOG_EVENT
     );
-    expect(eventSystem['subscribers'][0].handler).toBe(emptyHandler);
+    expect(eventSystem['subscribers'][0].handler).toBe(echoHandler);
     /* eslint-enable dot-notation */
   });
 
   test("Event System's unsubscribe() method works", () => {
     const eventSystem = EventSystem.getInstance();
 
-    eventSystem.subscribe(EVENT_SYSTEM_EVENT_NAMES.LOG_EVENT, emptyHandler);
-    eventSystem.unsubscribe(EVENT_SYSTEM_EVENT_NAMES.LOG_EVENT, emptyHandler);
+    eventSystem.subscribe(EVENT_SYSTEM_EVENT_NAMES.LOG_EVENT, echoHandler);
+    eventSystem.unsubscribe(EVENT_SYSTEM_EVENT_NAMES.LOG_EVENT, echoHandler);
 
     /* eslint-disable dot-notation */
     expect(eventSystem['subscribers'].length).toBe(0);
@@ -40,16 +38,16 @@ describe('Event System subscription worker', () => {
   test("Event System's resubscribe logic works", () => {
     const eventSystem = EventSystem.getInstance();
 
-    eventSystem.subscribe(EVENT_SYSTEM_EVENT_NAMES.LOG_EVENT, emptyHandler);
-    eventSystem.unsubscribe(EVENT_SYSTEM_EVENT_NAMES.LOG_EVENT, emptyHandler);
-    eventSystem.subscribe(EVENT_SYSTEM_EVENT_NAMES.LOG_EVENT, emptyHandler);
+    eventSystem.subscribe(EVENT_SYSTEM_EVENT_NAMES.LOG_EVENT, echoHandler);
+    eventSystem.unsubscribe(EVENT_SYSTEM_EVENT_NAMES.LOG_EVENT, echoHandler);
+    eventSystem.subscribe(EVENT_SYSTEM_EVENT_NAMES.LOG_EVENT, echoHandler);
 
     /* eslint-disable dot-notation */
     expect(eventSystem['subscribers'].length).toBe(1);
     expect(eventSystem['subscribers'][0].event).toBe(
       EVENT_SYSTEM_EVENT_NAMES.LOG_EVENT
     );
-    expect(eventSystem['subscribers'][0].handler).toBe(emptyHandler);
+    expect(eventSystem['subscribers'][0].handler).toBe(echoHandler);
     /* eslint-enable dot-notation */
   });
 });
