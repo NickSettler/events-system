c7d4991be199ad12c4c9edbe3535df3301c878ff
NickSettler <moiseevnikita14@gmail.com>
2022-07-29 22:51:55 +0200

    (refactor) refactors index export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
-import EventSystem from './event-system';
+import EventSystem, {
+  EVENT_SYSTEM_EVENT_NAMES,
+  EventSystemEvents,
+  EventSystemSubscriber,
+} from './event-system';
 
+export { EVENT_SYSTEM_EVENT_NAMES, EventSystemEvents, EventSystemSubscriber };
 export default EventSystem;
