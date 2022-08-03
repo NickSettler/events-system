814fe0ae0bbdf5e778d51af135582fb146feb931
NickSettler <moiseevnikita14@gmail.com>
2022-08-03 20:40:44 +0200

    (bugfix) delete event from buffer on unsubscribe

--- src/event-system.ts
+++ src/event-system.ts
@@ -167,6 +167,8 @@ export default class EventSystem {
       (subscriber: EventSystemSubscriber<EVENT_SYSTEM_EVENT_NAMES>) =>
         subscriber.event !== event || subscriber.handler !== handler
     );
+
+    this.buffer.delete(event);
   }
 
   /**
