a26dd3e5837d1b2d33fb75243243b92591bf8e24
NickSettler <moiseevnikita14@gmail.com>
2022-08-04 01:57:30 +0200

    (refactor) removes useless error class

--- src/errors/event-system-init-error.ts
+++
@@ -1,8 +0,0 @@
-export default class EventSystemInitError extends Error {
-  constructor(message?: string) {
-    super(message);
-    this.name = 'EventSystemInitError';
-
-    Object.setPrototypeOf(this, EventSystemInitError.prototype);
-  }
-}
